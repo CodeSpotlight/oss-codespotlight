7f3f9857d103ab441c7efbef544583c059cddbcb
Esther White <esthersoftwaredev@gmail.com>
2025-03-09 23:45:34 +0200

    current

--- src/app/components/apply/apply.component.ts
+++ src/app/components/apply/apply.component.ts
@@ -1,14 +1,26 @@
-import { Component, OnInit, signal } from "@angular/core";
+import { Component, inject, OnInit, signal } from "@angular/core";
 
 import { MatStepperModule } from "@angular/material/stepper";
+import { MatFormFieldModule } from "@angular/material/form-field";
+import { MatInputModule } from '@angular/material/input';
+import {MatSelectModule} from '@angular/material/select';
 import { MatRadioModule } from "@angular/material/radio";
 import { MatButtonModule } from "@angular/material/button";
 import { FormsModule } from "@angular/forms";
 import { Project } from "../../models/project";
+import { EmailService } from "../../services/email.service";
 
 @Component({
 	selector: "app-apply",
-	imports: [FormsModule, MatStepperModule, MatRadioModule, MatButtonModule],
+	imports: [
+		FormsModule,
+		MatStepperModule,
+		MatFormFieldModule,
+    MatInputModule,
+    MatSelectModule,
+		MatRadioModule,
+		MatButtonModule,
+	],
 	templateUrl: "./apply.component.html",
 	styleUrl: "./apply.component.scss",
 })
@@ -17,11 +29,18 @@ export class ApplyComponent implements OnInit {
 	selectedTech = "";
 	techOptions: string[] = [];
 	availableProjects: Project[] = [];
-  selectedProject: Project | null = null;
+	selectedProject: Project | null = null;
+  durationOptions = ["1 month", "2 months", "3 months", "4 months", "5 months", "6 months"];
+  selectedDuration = "";
+  fullName = "";
+  contactEmail = "";
+  message = "";
+  applicationSubmitted = false;
+  emailService = inject(EmailService);
 
-  ngOnInit() {
-    this.selectedProject = null;
-  }
+	ngOnInit() {
+		this.selectedProject = null;
+	}
 
 	updateTechOptions() {
 		if (this.selectedArea === "frontend") {
@@ -29,41 +48,63 @@ export class ApplyComponent implements OnInit {
 		} else if (this.selectedArea === "backend") {
 			this.techOptions = ["Node.js"];
 		} else {
-      this.techOptions = [];
-      this.availableProjects = [];
-      this.selectedProject = null;
-    }
+			this.techOptions = [];
+			this.availableProjects = [];
+			this.selectedProject = null;
+		}
 	}
 
 	updateAvailableProjects() {
 		if (this.selectedTech === "Angular") {
 			this.availableProjects = [
-				{ name: "Hashnode with Angular", url: "https://github.com/HashnodeWithAngular", image: "https://avatars.githubusercontent.com/u/161918167?s=200&v=4" },
+				{
+					name: "Hashnode with Angular",
+					url: "https://github.com/HashnodeWithAngular",
+					image: "https://avatars.githubusercontent.com/u/161918167?s=200&v=4",
+				},
 			];
 		} else if (this.selectedTech === "Next.js") {
-      this.availableProjects = [
-        { name: "Coming Soon", url: "https://google.com", image: "https://developers.elementor.com/docs/assets/img/elementor-placeholder-image.png" },
-      ];
-    } else if (this.selectedTech === "Node.js") {
-      this.availableProjects = [
-        { name: "Coming Soon", url: "https://google.com", image: "https://developers.elementor.com/docs/assets/img/elementor-placeholder-image.png" },
-      ];
-    } else {
-      this.availableProjects = [];
-      this.selectedProject = null;
-    }
+			this.availableProjects = [
+				{
+					name: "Coming Soon",
+					url: "https://google.com",
+					image:
+						"https://developers.elementor.com/docs/assets/img/elementor-placeholder-image.png",
+				},
+			];
+		} else if (this.selectedTech === "Node.js") {
+			this.availableProjects = [
+				{
+					name: "Coming Soon",
+					url: "https://google.com",
+					image:
+						"https://developers.elementor.com/docs/assets/img/elementor-placeholder-image.png",
+				},
+			];
+		} else {
+			this.availableProjects = [];
+			this.selectedProject = null;
+		}
 	}
 
-  setSelectedProject(project: Project) {
-    if (project.name !== "Coming Soon") {
-      this.selectedProject = project;
-      console.log(this.selectedProject);
-    } else {
-      this.selectedProject = null;
-    }
-  }
+	setSelectedProject(project: Project) {
+		if (project.name !== "Coming Soon") {
+			this.selectedProject = project;
+			console.log(this.selectedProject);
+		} else {
+			this.selectedProject = null;
+		}
+	}
 
-	apply() {
-		alert("Your application has been successfully submitted!");
+	submitApplication() {
+    this.emailService.submitApplicationEmail(
+      this.selectedArea,
+      this.selectedTech,
+      this.selectedProject!.name,
+      this.selectedDuration,
+      this.fullName,
+      this.contactEmail,
+      this.message
+    );
 	}
 }
---
+++ src/app/services/email.service.ts
@@ -0,0 +1,39 @@
+import { HttpClient } from "@angular/common/http";
+import { inject, Injectable, signal } from "@angular/core";
+
+@Injectable({
+	providedIn: "root",
+})
+export class EmailService {
+  applicationSubmitted = signal<boolean>(false);
+	http = inject(HttpClient);
+
+	submitApplicationEmail(
+		selectedArea: string,
+		selectedTech: string,
+		selectedProject: string,
+		selectedDuration: string,
+		fullName: string,
+		contactEmail: string,
+		message: string
+	) {
+		return this.http.post("https://server.codespotlight.dev/api/send-email", {
+			selectedArea,
+			selectedTech,
+			selectedProject,
+			selectedDuration,
+			fullName,
+			contactEmail,
+			message,
+		}).subscribe({
+      next: (response) => {
+        console.log("Email sent successfully:", response);
+        this.applicationSubmitted.set(true);
+      },
+      error: (error) => {
+        console.error("Failed to send email:", error);
+        this.applicationSubmitted.set(false);
+      },
+    });
+	}
+}
